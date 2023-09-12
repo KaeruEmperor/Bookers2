15e5168a0e84e0246919bc534d4120ce09a12534
KaeruEmperor <140334917+KaeruEmperor@users.noreply.github.com>
2023-09-12 12:15:12 +0000

    SecondCommit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about" => "homes#about", as: "about"
-  resources :books
+  resources :books do
+     resource :favorites, only: [:create, :destroy]
+  end
   resources :users, only:[:show, :edit, :update, :index]
   # For details on the DSL available within this file, se
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
